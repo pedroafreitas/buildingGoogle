919e6438f0027ecdf3a3d73a2741b50adb47f269
pedroafreitas <pedro.afd@discente.ufma.br>
2021-11-01 23:31:57 -0300

    logic finished

--- boolean/main.py
+++ boolean/main.py
@@ -1,49 +1,36 @@
-
+"""
+Pedro Arthur Freitas Dias
+Desenvolido em Python 3.9
+"""
 
 france = [1,2,3,4,5,7,8,9,11,12,13,14,15]
-# paris = [2,6,10,12,15]
-# lear = [12,15]
-paris = [1, 4, 12, 13]
-lear = [2, 3, 4, 13]
+paris = [2,6,10,12,15]
+lear = [12,15]
+
 
 def lastValue(list, index):
     if index == (len(list) - 1):
         return True
     else: return False
 
-def union(p1, p2): #or   
-    answer = []
-    i = 0
-    j = 0
-    while p1[-1] != float('inf') or p2[-1] != float('inf'):
-        if p1[i] == p2[j]:
-            answer.append(p1[i]) 
+def removeDuplicates(p1):
+    p1 = list(dict.fromkeys(p1))
+    return p1
 
-            if lastValue(p1, i) and lastValue (p2, j):
-                p1[i] = float('inf')
-                p2[j] = float('inf')
-            elif lastValue(p1, i):
-                p1[i] = float('inf')
-                j += 1
-            elif lastValue(p2, j):
-                p2[j] = float('inf')
-                i += 1
-            else:
-                i += 1
-                j += 1
-        elif p1[i] < p2[j]:
-            answer.append(p1[i]) 
-            if lastValue(p1, i): 
-                p1[i] = float('inf')
-            else: i += 1
-        else:
-            answer.append(p2[j]) 
-            if lastValue(p2, j): 
-                p2[j] = float('inf')
-            else: j += 1
+def difference(target,subtract): 
+    answer = []
+    for i in range(len(target)):
+        if target[i] not in subtract:
+            answer.append(target[i])
     return answer
 
-def intersect(p1, p2): #and
+def union(p1, p2):    
+    answer = p1 + p2
+    answer.sort()
+    answer = removeDuplicates(answer)
+    return answer
+
+def intersect(p1, p2): 
     answer = []
     i = 0
     j = 0
@@ -60,8 +47,8 @@ def intersect(p1, p2): #and
 
 
 
-def complement(): #not
-    answer = []
-    return answer
+parisAndNotFrance = difference(paris,france)
+print("PARIS and not FRANCE: " + str(parisAndNotFrance))
 
-print(union(lear, paris))
+orLear = union(parisAndNotFrance, lear)
+print("(PARIS and not FRANCE) or LEAR: " + str(orLear))
